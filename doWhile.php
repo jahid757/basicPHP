87930a97aad38d70543e08e553ceebf294d11ac9
Jahid Hasan <slniloy500@gmail.com>
2021-08-16 07:52:31 +0600

    create while & do while loop

---
+++ doWhile.php
@@ -0,0 +1,8 @@
+<?php
+
+$i = 0;
+
+do{
+    echo "<h2> $i. Hi, I am Jahid Hasan. I love Backend</h2>";
+    $i++;
+}while($i <= 7);
---
+++ while.php
@@ -0,0 +1,17 @@
+<?php
+
+$i = 0;
+
+while($i <= 10){
+    echo "<h2> $i. Hi, I am Jahid Hasan. I love Backend</h2>";
+    $i+= 2;
+}
+
+echo "<br><br><br><br><br>";
+
+$j = 10;
+
+while($j >= 0){
+    echo "<h2> $j. Hi, I am Jahid Hasan. I love Backend</h2>";
+    $j--;
+}
